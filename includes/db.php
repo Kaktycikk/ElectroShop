0d889d656247e3d69e319945110756ca50a33aef
Kseniya Travkina <trkseniya342@gmail.com>
2026-06-25 23:21:01 +0400

    Fix db env

--- includes/db.php
+++ includes/db.php
@@ -1,6 +1,19 @@
 <?php
 
-$env = parse_ini_file(__DIR__ . "/../.env");
+if (file_exists(__DIR__ . "/../.env"))
+{
+    $env = parse_ini_file(__DIR__ . "/../.env");
+}
+else
+{
+    $env = [
+        "DB_HOST" => getenv("DB_HOST"),
+        "DB_PORT" => getenv("DB_PORT"),
+        "DB_NAME" => getenv("DB_NAME"),
+        "DB_USER" => getenv("DB_USER"),
+        "DB_PASSWORD" => getenv("DB_PASSWORD"),
+    ];
+}
 
 $conn = pg_connect(
     "host={$env["DB_HOST"]} " .
