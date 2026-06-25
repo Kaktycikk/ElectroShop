92d5662eebe1e8eb1655018a304b6a1c86f0ee90
Kseniya Travkina <trkseniya342@gmail.com>
2026-06-26 02:00:45 +0400

    update

--- includes/site_header.php
+++ includes/site_header.php
@@ -23,7 +23,7 @@ $currentPage = basename($_SERVER['PHP_SELF']);
           href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
     <link rel="stylesheet"
-          href="/../public/css/site.css">
+          href="/css/site.css">
 
 </head>
 
