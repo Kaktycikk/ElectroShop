2d667f4b2841ba6dcd53dce4f601343c2ffbb689
Kseniya Travkina <trkseniya342@gmail.com>
2026-06-25 23:45:06 +0400

    Debug Railway env

--- includes/db.php
+++ includes/db.php
@@ -1,29 +1,18 @@
 <?php
 
-if (file_exists(__DIR__ . "/../.env"))
-{
-    $env = parse_ini_file(__DIR__ . "/../.env");
-}
-else
-{
-    $env = [
-        "DB_HOST" => getenv("DB_HOST"),
-        "DB_PORT" => getenv("DB_PORT"),
-        "DB_NAME" => getenv("DB_NAME"),
-        "DB_USER" => getenv("DB_USER"),
-        "DB_PASSWORD" => getenv("DB_PASSWORD"),
-    ];
-}
+var_dump(getenv("PGHOST"));
+var_dump(getenv("PGDATABASE"));
+die();
 
 $conn = pg_connect(
-    "host={$env["DB_HOST"]} " .
-    "port={$env["DB_PORT"]} " .
-    "dbname={$env["DB_NAME"]} " .
-    "user={$env["DB_USER"]} " .
-    "password={$env["DB_PASSWORD"]}"
+    "host=" . getenv("PGHOST") .
+    " port=" . getenv("PGPORT") .
+    " dbname=" . getenv("PGDATABASE") .
+    " user=" . getenv("PGUSER") .
+    " password=" . getenv("PGPASSWORD")
 );
 
 if (!$conn)
 {
-    die("Ошибка подключения к БД");
+    die(pg_last_error());
 }
